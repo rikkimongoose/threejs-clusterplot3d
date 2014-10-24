f8ae52fcb5d1c290685a30769e032405a0938b6a
rikkimongoose <alexander.a.teut@gmail.com>
2014-10-24 15:53:08 +0400

    ~ new draft

--- js/THREEx.Plot3d.js
+++ js/THREEx.Plot3d.js
@@ -1,10 +1,12 @@
 var ThreeJs_plots = [];
 
-function drawPlot(elem_id, config, data, onitemloaded, onloaded){
+function plot3d(elem_id, extract_rules_config, plot_view_config, data, onitemloaded, onloaded){
 	var plot = {
 		elem_id : elem_id,
-		config : config,
+		extract_rules_config : extract_rules_config,
+		plot_view_config : plot_config,
 		data : data,
+		data_config : null,
 		itemloaded : [],
 		loaded : []
 	};
@@ -17,6 +19,24 @@ function drawPlot(elem_id, config, data, onitemloaded, onloaded){
 		plot.loaded.push(onloaded);
 	}
 
+	plot.elem = document.getElementById(elem_id);
+	if(!plot.elem){
+		console.error("Element with id '%s' is not found.", elem_id);
+		return;
+	}
+
+	plot.extract_rules = extract_rules_config(plot.extract_rules_config);
+
+	plot.data_items = extract_data(plot.data, plot,extract_rules)
+
 	THREE_plots.push(plot);
 	return plot;
+}
+
+function compile_extract_rules(config){
+
+}
+
+function extract_data(data, rules){
+
 }
