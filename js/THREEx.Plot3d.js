3009725570db62a3e73471f361c089188ad2832d
RikkiMongoose <alexander.a.teut@gmail.com>
2014-10-27 01:37:03 +0300

    add stratch

--- js/THREEx.Plot3d.js
+++ js/THREEx.Plot3d.js
@@ -9,21 +9,24 @@ THREEx.ClusterPlot3d = function(plot_options) {
 	this.keyboard = new THREEx.KeyboardState();
 	this.clock = new THREE.Clock();
 
-		var get_rule_value = function(rule_key) {
-			if(typeof rules[rule_key] != "undefined"){
-				if(typeof rules[rule_key] == "function")
-					return rules[rule_key];
-				return function(item) { return item[rules[rule_key]]; };
-			}
-			var rule_key_const = rule_key + "-const";
-			if(typeof rules[rule_key_const] != "undefined")
-				return function() { return rules[rule_key_const] };
-			if(typeof default_rules[rule_key] != "undefined")
-				return default_rules[rule_key];
-			return null;
+	var get_rule_value = function(rule_key) {
+		if(typeof rules[rule_key] != "undefined"){
+			if(typeof rules[rule_key] == "function")
+				return rules[rule_key];
+			return function(item) { return item[rules[rule_key]]; };
 		}
+		var rule_key_const = rule_key + "-const";
+		if(typeof rules[rule_key_const] != "undefined")
+			return function() { return rules[rule_key_const] };
+		if(typeof default_rules[rule_key] != "undefined")
+			return default_rules[rule_key];
+		return null;
+	}
+
 	this.options = plot_options || {};
 	this.options.show_stats = false;
+	this.options.stratch = true;
+	this.options.show_grids = true;
 
 	if(typeof THREEx._plots3d == "undefined")	{
 		THREEx._plots3d = [];
@@ -74,60 +77,6 @@ THREEx.ClusterPlot3d = function(plot_options) {
 					HOVER : 2
 				}
 			}
-		};
-
-	this.sceneConfig = {
-			show_axes : true,
-			scene_objects : [],
-			lighting : {},
-			camera : {},
-			data : [
-				{
-					position : 
-					{
-						x : 0,
-						y : 0,
-						z : 0
-					},
-					color : 0x00ff00,
-					outline_color : 0xff0000,
-					outline_expand : 1.2,
-					size : 10,
-					type : PLOT_TYPE.ITEM.CUBE,
-					material : PLOT_TYPE.MATERIAL.PHONG,
-					original_item : {}
-				},
-				{
-					position : 
-					{
-						x : 40,
-						y : 40,
-						z : 40
-					},
-					color : 0x0000ff,
-					outline_color : 0xff0000,
-					outline_expand : 1.2,
-					size : 5,
-					type : PLOT_TYPE.ITEM.BAR,
-					material : PLOT_TYPE.MATERIAL.BASIC,
-					original_item : {}
-				},
-				{
-					position : 
-					{
-						x : 0,
-						y : 40,
-						z : 40
-					},
-					color : 0x0000ff,
-					outline_color : 0xff0000,
-					outline_expand : 1.2,
-					size : 8,
-					type : PLOT_TYPE.ITEM.SPHERE,
-					material : PLOT_TYPE.MATERIAL.LAMBER,
-					original_item : {}
-				}
-			]
 		};
 
 	this.init = function(container) {
@@ -198,7 +147,10 @@ THREEx.ClusterPlot3d = function(plot_options) {
 		this.renderer.render( this.scene, this.camera );
 	}
 
-	this.grid_params = {
+	this.options.steps_count = 20;
+	this.options.steps_count_koeff = 10;
+
+	this.grid_options = {
 		xz : {
 			size : 100,
 			step : 10
@@ -211,21 +163,21 @@ THREEx.ClusterPlot3d = function(plot_options) {
 			size : 100,
 			step : 10
 		}
-	}
+	};
 
 	this.grids = function(){
-		var gridXZ = new THREE.GridHelper(this.grid_params.xz.size, this.grid_params.xz.step);
+		var gridXZ = new THREE.GridHelper(this.grid_options.xz.size, this.grid_options.xz.step);
 		gridXZ.setColors( new THREE.Color(0x006600), new THREE.Color(0x006600) );
 		gridXZ.position.set( 100,0,100 );
 		this.scene.add(gridXZ);
 			
-		var gridXY = new THREE.GridHelper(this.grid_params.xy.size, this.grid_params.xy.step);
+		var gridXY = new THREE.GridHelper(this.grid_options.xy.size, this.grid_options.xy.step);
 		gridXY.position.set( 100,100,0 );
 		gridXY.rotation.x = Math.PI/2;
 		gridXY.setColors( new THREE.Color(0x000066), new THREE.Color(0x000066) );
 		this.scene.add(gridXY);
 
-		var gridYZ = new THREE.GridHelper(this.grid_params.yz.size, this.grid_params.yz.step);
+		var gridYZ = new THREE.GridHelper(this.grid_options.yz.size, this.grid_options.yz.step);
 		gridYZ.position.set( 0,100,100 );
 		gridYZ.rotation.z = Math.PI/2;
 		gridYZ.setColors( new THREE.Color(0x660000), new THREE.Color(0x660000) );
@@ -244,9 +196,9 @@ THREEx.ClusterPlot3d = function(plot_options) {
 		}
 
 		if(item_type == PLOT_TYPE.ITEM.SPHERE)
-			return getSphereGeometry(size / 2);
+			return getSphereGeometry(size);
 		else if(item_type == PLOT_TYPE.ITEM.CUBE)
-			return getCubeGeometry(size);
+			return getCubeGeometry(size * 2);
 		else if(item_type == PLOT_TYPE.ITEM.BAR)
 			return getBarGeometry(size, position);
 
@@ -278,26 +230,26 @@ THREEx.ClusterPlot3d = function(plot_options) {
 	}
 	
 	this.draw_plot = function() {
-		var itemDataIndex = this.sceneConfig.data.length;
-		while(itemDataIndex--) {
-			var itemData = this.sceneConfig.data[itemDataIndex];
-			var geometry = this.getGeometry(itemData.type, itemData.size, itemData.position);
-			var material = this.getMaterial(itemData.material, itemData.color);
+		var item_data_index = this.parsed_data.length;
+		while(item_data_index--) {
+			var item_data = this.parsed_data[item_data_index];
+			var geometry = this.getGeometry(item_data.type, item_data.size, item_data);
+			var material = this.getMaterial(item_data.material, item_data.color);
 			var mesh = new THREE.Mesh( geometry, material );
 
-			if(itemData.type == PLOT_TYPE.ITEM.BAR)
-				mesh.position.set(itemData.position.x,( itemData.position.y - itemData.size / 2) / 2, itemData.position.z);
+			if(item_data.type == PLOT_TYPE.ITEM.BAR)
+				mesh.position.set(item_data.x,( item_data.y - item_data.size / 2) / 2, item_data.z);
 			else
-				mesh.position.set(itemData.position.x, itemData.position.y, itemData.position.z);
+				mesh.position.set(item_data.x, item_data.y, item_data.z);
 			this.scene.add(mesh);
 
-			if(itemData.outline_color && itemData.outline_expand) {
-				var outlineMaterial = new THREE.MeshBasicMaterial( { color: itemData.outline_color, side: THREE.BackSide } );
+			if(item_data.outline_color && item_data.outline_expand) {
+				var outlineMaterial = new THREE.MeshBasicMaterial( { color: item_data.outline_color, side: THREE.BackSide } );
 				var outlineMesh = new THREE.Mesh( geometry, outlineMaterial );
 				outlineMesh.position.x = mesh.position.x;
 				outlineMesh.position.y = mesh.position.y;
 				outlineMesh.position.z = mesh.position.z;
-				outlineMesh.scale.multiplyScalar(itemData.outline_expand);
+				outlineMesh.scale.multiplyScalar(item_data.outline_expand);
 				this.scene.add( outlineMesh );
 			}
 		}
@@ -306,43 +258,59 @@ THREEx.ClusterPlot3d = function(plot_options) {
 	var PARSE_RULES_TYPES = {
 		NUMERIC : 0,
 		COLOR : 1,
-		FIGURE : 2
+		FIGURE : 2,
+		MATERIAL : 3
 	};
 
 	this.parse_rules = {
 			x : {
 				type : PARSE_RULES_TYPES.NUMERIC,
-				func : null
+				func : null,
+				is_normalised : true
 			},
 			y : {
 				type : PARSE_RULES_TYPES.NUMERIC,
-				func : null
+				func : null,
+				is_normalised : true
 			},
 			z : {
 				type : PARSE_RULES_TYPES.NUMERIC,
-				func : null
+				func : null,
+				is_normalised : true
 			},
 			color : {
 				type : PARSE_RULES_TYPES.COLOR,
-				func : null
+				func : null,
+				is_normalised : false
 			},
 			outline_color : {
 				type : PARSE_RULES_TYPES.COLOR,
-				func : null
+				func : null,
+				is_normalised : false
 			},
 			outline_expand : {
 				type : PARSE_RULES_TYPES.NUMERIC,
-				func : null
+				func : null,
+				is_normalised : false
+			},
+			material : {
+				type : PARSE_RULES_TYPES.MATERIAL,
+				func : null,
+				is_normalised : false
 			},
 			size : {
 				type : PARSE_RULES_TYPES.NUMERIC,
-				func : null
+				func : null,
+				is_normalised : false
 			},
 			type : {
 				type : PARSE_RULES_TYPES.FIGURE,
-				func : null
+				func : null,
+				is_normalised : false
 			}
 		};
+
+	this.parse_rules_data_columns = [];
 
 	this.prepare_parse_rules = function(rules) {
 		var default_rules_values = {
@@ -352,6 +320,7 @@ THREEx.ClusterPlot3d = function(plot_options) {
 			color : 0xFF0000,
 			outline_color : null,
 			outline_expand : 1.2,
+			material : PLOT_TYPE.MATERIAL.LAMBER,
 			size : 1,
 			type : PLOT_TYPE.ITEM.SPHERE
 		};
@@ -362,26 +331,30 @@ THREEx.ClusterPlot3d = function(plot_options) {
 			color : function() { return default_rules_values.color; },
 			outline_color : function() { return default_rules_values.outline_color; },
 			outline_expand : function() { return default_rules_values.outline_expand; },
+			material : function() { return default_rules_values.material; },
 			size : function(item) { return (typeof item[3] != "undefined") ? item[3] : default_rules_values.size; },
 			type : function() { return PLOT_TYPE.ITEM.SPHERE; }
 		};
 
-		var get_rule_value = function(rule_key) {
+		var get_rule_value = function(rule_key, is_normalised) {
 			if(typeof rules[rule_key] != "undefined"){
 				if(typeof rules[rule_key] == "function")
 					return rules[rule_key];
-				return function(item) { return item[rules[rule_key]]; };
+				return { func: function(item) { return item[rules[rule_key]]; }, is_changed : true };
 			}
 			var rule_key_const = rule_key + "-const";
 			if(typeof rules[rule_key_const] != "undefined")
-				return function() { return rules[rule_key_const] };
+				return { func : function() { return rules[rule_key_const] }, is_changed : false };
 			if(typeof default_rules[rule_key] != "undefined")
-				return default_rules[rule_key];
+				return { func : default_rules[rule_key], is_changed : is_normalised };
 			return null;
 		}
 
 		for(var rule_key in default_rules) {
-			this.parse_rules[rule_key].func = get_rule_value(rule_key);
+			var rule_value = get_rule_value(rule_key, this.parse_rules[rule_key].is_normalised);
+			this.parse_rules[rule_key].func = rule_value.func;
+			if(rule_value.is_changed)
+				this.parse_rules_data_columns.push(rule_key);
 		}
 	}
 
@@ -396,15 +369,54 @@ THREEx.ClusterPlot3d = function(plot_options) {
 		}
 		
 		this.init();
-		this.grids();
+		if(this.options.show_grids){
+			this.grids();
+		}
 		this.animate();
 
 		return this.scene;
 	}
 
+	this.normalise_parsed_data = function(){
+		var normalise_numeric = function(data_to_normalise, key, steps_count, steps_count_koeff) {
+			var maxValue = Number.NEGATIVE_INFINITY;
+			var minValue = Number.POSITIVE_INFINITY;
+			var data_length = data_to_normalise.length;
+			var data_key = data_length;
+			while(data_key--) {
+				var data_item = data_to_normalise[data_key][key];
+				maxValue = Math.max(data_item, maxValue);
+				minValue = Math.min(data_item, minValue);
+			}
+
+			var koeff = steps_count / (maxValue - minValue) ;
+			data_key = data_length;
+			while(data_key--) {
+				var data_item = data_to_normalise[data_key][key];
+				data_item = (data_item - minValue) * koeff * steps_count_koeff;
+				data_to_normalise[data_key][key] = data_item;
+			}
+		};
+		var normalise_color = function(data_to_normalise, key) {
+			/* TODO :  palettes like R */
+		};
+
+		for(var rule_key in this.parse_rules_data_columns) {
+			var data_column_key = this.parse_rules_data_columns[rule_key];
+			var rule = this.parse_rules[data_column_key];
+			if(rule.type == PARSE_RULES_TYPES.NUMERIC){
+				normalise_numeric(this.parsed_data, data_column_key, this.options.steps_count, this.options.steps_count_koeff);
+			} else if (rule.type == PARSE_RULES_TYPES.COLOR){
+				normalise_color(this.parsed_data, data_column_key);
+			}
+		}
+	}
+
 	this.parse_data = function(data) {
 		this.source_data = data;
+		this.raw_parsed_data = [];
 		this.parsed_data = [];
+
 		var source_data_length = data.length;
 		for(var i = 0; i < source_data_length; i++){
 			var source_data_item = this.source_data[i];
@@ -413,7 +425,11 @@ THREEx.ClusterPlot3d = function(plot_options) {
 				var rule = this.parse_rules[rule_key];
 				parsed_data_item[rule_key] = rule.func(source_data_item);
 			}
+			this.raw_parsed_data.push(parsed_data_item);
 			this.parsed_data.push(parsed_data_item);
+		}
+		if(this.options.stratch){
+			this.normalise_parsed_data(this.parsed_data);
 		}
 		console.log(this.parsed_data);
 	}
@@ -436,7 +452,7 @@ THREEx.getClusterPlotById = function(plot_id) {
 	if(typeof THREEx._plots3d == "undefined")
 		return null;
 	var plot_index = THREEx._plots3d.length;
-	while(--plot_index){
+	while(plot_index--){
 		var plot_item = THREEx._plots3d[plot_index];
 		if(typeof plot_index.id != "underfined" && plot_index.id == plot_id)
 			return plot_index;
