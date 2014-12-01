761b053b4bb55f802d6cbe0c5f80898605d6db74
rikkimongoose <alexander.a.teut@gmail.com>
2014-12-01 16:12:12 +0300

    + add draft of particles grouping

--- demo/01-simple-geometry/js/THREEx.Plot3d.js
+++ demo/01-simple-geometry/js/THREEx.Plot3d.js
@@ -355,12 +355,9 @@ THREEx.ClusterPlot3d = function(plot_options) {
 
 		switch(item_type)
 		{
-			case THREEx.PLOT_TYPE.ITEM.SPHERE:
-				return getSphereGeometry(size);
-			case THREEx.PLOT_TYPE.ITEM.CUBE:
-				return getCubeGeometry(size * 2);
-			case THREEx.PLOT_TYPE.ITEM.BAR:
-				return getBarGeometry(size, position);
+			case THREEx.PLOT_TYPE.ITEM.SPHERE : return getSphereGeometry(size);
+			case THREEx.PLOT_TYPE.ITEM.CUBE   : return getCubeGeometry(size * 2);
+			case THREEx.PLOT_TYPE.ITEM.BAR    : return getBarGeometry(size, position);
 		}
 
 		return null;
@@ -381,12 +378,9 @@ THREEx.ClusterPlot3d = function(plot_options) {
 
 		switch(item_material)
 		{
-			case THREEx.PLOT_TYPE.MATERIAL.BASIC:
-				return getBasicMaterial( { color: item_color } );
-			case THREEx.PLOT_TYPE.MATERIAL.LAMBER:
-				return getLambertMaterial( { color: item_color } );
-			case THREEx.PLOT_TYPE.MATERIAL.PHONG:
-				return getPhongMaterial( { color: item_color } );
+			case THREEx.PLOT_TYPE.MATERIAL.BASIC  : return getBasicMaterial( { color: item_color } );
+			case THREEx.PLOT_TYPE.MATERIAL.LAMBER : return getLambertMaterial( { color: item_color } );
+			case THREEx.PLOT_TYPE.MATERIAL.PHONG  : return getPhongMaterial( { color: item_color } );
 		}
 		return null;
 	}
@@ -450,12 +444,12 @@ THREEx.ClusterPlot3d = function(plot_options) {
 		}
 
 		function draw_plot_particle(plot, parsed_data) {
-			var material = new THREE.PointCloudMaterial( { size: 4, vertexColors: THREE.VertexColors } );
 
-			function getParticleMaterials(item_data){
+			function createParticleMaterial(item_data){
 			 	return {
 			 		color : item_data.color,
-			 		size  : item_data.size
+			 		size  : item_data.size,
+			 		items : []
 			 	};
 			}
 
@@ -466,32 +460,49 @@ THREEx.ClusterPlot3d = function(plot_options) {
 					&& m1.size  == m2.size;
 			}
 
-			var item_data_index = parsed_data.length;
+			function getParticleMaterial(particle_classes, item_data)
+			{
+				var index = particle_classes.length;
+				while(index--){
+					var particle_class = particle_classes[index];
+					if(cmpParticleMaterials(particle_class, item_data)
+						return particle_class;
+				}
+				var new_material = createParticleMaterial(item_data);
+				particle_classes.push(new_material);
+				return new_material;
+			}
 
+			function do_show_patricles(plot, item_data_sorted){
+				for(var i = 0, li = item_data_sorted.length; i < li; i++) {
+					var particle_class = item_data_sorted[i];
+					var geometry = new THREE.Geometry();
+					var material = new THREE.PointCloudMaterial({ size: particle_class.size, vertexColors: THREE.VertexColors });
+					for(var j = 0, lj = particle_class.items.length; j < lj; j++) {
+						var particle_class_item_data = particle_class.items[j];
+						var particle = new THREE.Vector3(
+								particle_class_item_data.x,
+								particle_class_item_data.y,
+								particle_class_item_data.z
+							);
+						geometry.vertices.push(particle);
+						plot.execEvent("onItemLoad", { item : item_data });
+					}
+				}
+				var system = new THREE.PointCloud(geometry, material);
+				plot.scene.add(system);
+			}
+
+			var item_data_index = parsed_data.length;
 			var item_data_sorted = [];
 
 			while(item_data_index--) {
 				var item_data = parsed_data[item_data_index];
-				var item_data_material = getParticleMaterials(item_data);
-				
-			}
-			for(var i = 0, li = item_data_sorted.length; i < li; i++) {
-				var particle_class = item_data_sorted[i];
-				var geometry = new THREE.Geometry();
-				for(var j = 0, lj = particle_class.items.length; j < lj; j++) {
-					var particle_class_item_data = particle_class.items[j];
-					var particle = new THREE.Vector3(
-							particle_class_item_data.x,
-							particle_class_item_data.y,
-							particle_class_item_data.z
-						);
-					geometry.vertices.push(particle);
-				}
+				var item_data_material = getParticleMaterials(item_data_sorted, item_data);
+				item_data_material.items.push(item_data);
 			}
 
-			var system = new THREE.PointCloud(geometry, material);
-			plot.scene.add(system);
-			plot.execEvent("onItemLoad", { item : item_data });
+			do_show_patricles(plot, item_data_sorted);
 		}
 
 		var drawing_func = null;
